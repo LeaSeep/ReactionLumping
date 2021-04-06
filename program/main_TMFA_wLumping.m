d6465e9224abdf1a0c21931ff42504e9e32429ef
DESKTOP-LGI5TOJ\lease <leaseep@gmail.com>
2021-04-06 17:48:21 +0200

    FIX ATPM to 7.6 - was allowed to vary before

--- program/main_TMFA_wLumping.m
+++ program/main_TMFA_wLumping.m
@@ -1,7 +1,7 @@
 %% Perform Variability analysis of the DeltaGs before and after lumping
 % choose model: Ecoli or Bacillus
 clear
-modelChoice="Bacillus";
+modelChoice="Ecoli";
 %% SetUp the workspace
 % Load CobraToolbox; code was test with version: 2.29.2
 % initCobraToolbox(false)
@@ -10,12 +10,14 @@ cd("..\program")
 if(modelChoice=="Ecoli")
     fprintf("Choosen model: 'Ecoli'\n")
     fullModel=ReadIn_iJ904();
-    fullModel=computationalMedia(fullModel,'Reed_aerobicGlucose');    
+    fullModel=computationalMedia(fullModel,'Reed_aerobicGlucose');  
+    bigM=10^9;
 elseif modelChoice=="Bacillus"
     fprintf("Choosen model: 'Bacillus'\n")
     fullModel=ReadIn_Bacillus(); 
     fullModel.description='Bacillus';
     fullModel=computationalMedia(fullModel,'Henry_Glucose');
+    bigM=10^12;
 else 
     error("Choose either 'Ecoli' or 'Bacillus'")
 end
@@ -36,7 +38,7 @@ fullModel.lb(findRxnIDs(fullModel,findIntracellularRxns(fullModel)))=-1000;
 fullModel.ub(findRxnIDs(fullModel,findIntracellularRxns(fullModel)))=1000;
 fullModel.ub(findExcRxns(fullModel)==1)=1000;
 fullModel.lb(fullModel.c==1)=0; %BioMass
-
+fullModel.ub(247)=7.6;%ATPM
 %Assign all reactions to be reversible
 blockedRxns=findBlockedReaction(fullModel);
 fullModel.lb(findRxnIDs(fullModel,blockedRxns))=0;
@@ -64,8 +66,8 @@ idx.toBelumped=find((isnan(fullModel.DeltaG_m_std)==1));
 % violation. All done while assuring TMFA constraints as well as an
 % optimality constraint (based on FBA value)
 % This is done for the model with and without Lumping
-[conc_boundaries_withLump,x0_withLump]=getBoundaries(fullModel,10^12,idx.toBelumped,optVal,percentage);
-[conc_boundaries_withoutLump,x0_withoutLump]=getBoundaries(fullModel,10^12,[],optVal,percentage);
+[conc_boundaries_withLump,x0_withLump]=getBoundaries(fullModel,bigM,idx.toBelumped,optVal,percentage);
+%[conc_boundaries_withoutLump,x0_withoutLump]=getBoundaries(fullModel,bigM,[],optVal,percentage);
 
 %% Run TMFA & Variability analysis for Delta G
 for type=["withLumping","withOutLumping"]
@@ -74,18 +76,19 @@ for type=["withLumping","withOutLumping"]
     if all(type=='withLumping')
         fullModel.conc_lb=conc_boundaries_withLump.lb;
         fullModel.conc_ub=conc_boundaries_withLump.ub;
-        [Distr, optValTMFA, STAT, usedModel]=TMFA_LS(fullModel,10^12,idx.toBelumped,x0_withLump,optVal,percentage);
+        [Distr, optValTMFA, STAT, usedModel]=TMFA_LS(fullModel,bigM,idx.toBelumped,x0_withLump,optVal,percentage);
         rangesTable=myDeltaG_VA(usedModel,optValTMFA);
-        savingName1=cell2mat({'../result/' str2mat(modelChoice) '_' str2mat(type) '_' 'Ranges.csv'});
-        savingName2=cell2mat({'../result/' str2mat(modelChoice) '_' str2mat(type) '_' 'RxnsNames.csv'});
+        savingName1=cell2mat({'../result/' str2mat(modelChoice) '_' str2mat(type) '_' 'Ranges_alternativeSolution.csv'});
+        savingName2=cell2mat({'../result/' str2mat(modelChoice) '_' str2mat(type) '_' 'RxnsNames_alternativeSolution.csv'});
         writematrix(rangesTable,savingName1)
         writecell(fullModel.rxns,savingName2)
     else
         fullModel.conc_lb=conc_boundaries_withoutLump.lb;
         fullModel.conc_ub=conc_boundaries_withoutLump.ub;
-        [Distr, optValTMFA, STAT, usedModel]=TMFA_LS(fullModel,10^12,[],x0_withoutLump,optVal,percentage);
-        savingName1=cell2mat({'../result/' str2mat(modelChoice) '_' str2mat(type) '_' 'Ranges.csv'});
-        savingName2=cell2mat({'../result/' str2mat(modelChoice) '_' str2mat(type) '_' 'RxnsNames.csv'});
+        [Distr, optValTMFA, STAT, usedModel]=TMFA_LS(fullModel,bigM,[],x0_withoutLump,optVal,percentage);
+        rangesTable=myDeltaG_VA(usedModel,optValTMFA);
+        savingName1=cell2mat({'../result/' str2mat(modelChoice) '_' str2mat(type) '_' 'Ranges_alternativeSolution.csv'});
+        savingName2=cell2mat({'../result/' str2mat(modelChoice) '_' str2mat(type) '_' 'RxnsNames_alternativeSolution.csv'});
         writematrix(rangesTable,savingName1)
         writecell(fullModel.rxns,savingName2)
     end
